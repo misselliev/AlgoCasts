0a674ee3213ba299fb75f079143577b1520750ac
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-06 14:23:35 -0700

    finding most common char in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let charMax = '';
+
+  for (let char of str) {
+    if (!charMap) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      charMax = char;
+    }
+  }
+  return charMax;
+}
 
 module.exports = maxChar;
