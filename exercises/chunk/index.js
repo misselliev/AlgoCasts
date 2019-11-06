6e963898ad5c242b555cf444486587cae029d1c2
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-06 16:52:49 -0700

    split a big array into chunks

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   const result = [];
+//   for (let item of array) {
+//     const last = result[result.length - 1];
+//     if (!last || last.length === size) {
+//       result.push([item]);
+//     } else {
+//       last.push(item);
+//     }
+//   }
+//   return result;
+// }
+
+function chunk(array, size) {
+  const result = [];
+  let index = 0;
+  while (index < array.length) {
+    result.push(array.slice(index, index + size));
+    index += size;
+  }
+  return result;
+}
 
 module.exports = chunk;
