f9c653535fab4412d2f73ad9e3506a3dc1d5b4ef
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-06 10:58:22 -0700

    palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  const reverse = str
+    .split('')
+    .reverse()
+    .join('');
+  return reverse === str;
+}
+
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - i - 1];
+//   });
+// }
 
 module.exports = palindrome;
