1e9172f636c30d9db2de5cb5bf602f60ecd19ed0
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-01-06 14:15:47 -0700

    Build a pyramid from a given N number

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const mid = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (mid - row <= column && mid + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
